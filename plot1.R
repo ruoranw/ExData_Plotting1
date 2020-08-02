350f8abb930f0232b2e44704feb70e4f4212dca0
Ruoran Wang <ruoran0502@gmail.com>
2020-08-02 16:36:06 -0700

    modify plot1 R script to add comments.

--- plot1.R
+++ plot1.R
@@ -1,13 +1,14 @@
 library(data.table)
 library(dplyr)
 
+# Read data
 mydat <- read.table("household_power_consumption.txt", header = T, sep = ";", stringsAsFactors = F)
-class(mydat$Date)
 
+# Turn Date variable as class Date
 mydat$Date <- as.Date(mydat$Date,format="%d/%m/%Y")
-class(mydat$Date)
-head(mydat)
 
+#Subset the datain 2007-02-01 and 2007-02-02
 date_subsetted <- subset(mydat, Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02"))
 
+#Plotting
 hist(as.numeric(date_subsetted$Global_active_power), main ="Global Active Power", xlab = "Global Active Power(kilowatts)", col = "red" )
